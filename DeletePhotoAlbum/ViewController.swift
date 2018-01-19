401a622e1e9b94d01962cf92b546e3f4954d3257
kz56cd <kz56cd@gmail.com>
2018-01-19 14:58:59 +0900

    Add policies

--- DeletePhotoAlbum/ViewController.swift
+++ DeletePhotoAlbum/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+        let list = PHAssetCollection.fetchAssetCollections(
+            with: .album,
+            subtype: .any, options: nil)
+        print("👽 \(deleteButtonTapped)")
+    }
 }
 
