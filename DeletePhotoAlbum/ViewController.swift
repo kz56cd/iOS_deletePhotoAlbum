7ec8fd23e61f49f9334471969956a927d6aaf2ad
kz56cd <kz56cd@gmail.com>
2018-01-19 15:58:00 +0900

    [WIP] Add deleting album (bud code: because use magic number)

--- DeletePhotoAlbum/ViewController.swift
+++ DeletePhotoAlbum/ViewController.swift
@@ -20,12 +20,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    
     @IBAction func deleteButtonTapped(_ sender: Any) {
         let list = PHAssetCollection.fetchAssetCollections(
             with: .album,
-            subtype: .any, options: nil)
-        print("👽 \(deleteButtonTapped)")
+            subtype: .any, options: nil
+        )
+        var albums: NSMutableArray = []
+        list.enumerateObjects { album, index, isStop in
+//            let title = String(describing: album.localizedTitle)
+//            print(title)
+//            if title == String(describing: "Lovegreen") {
+//                albums.add(album)
+//            }
+            
+            if index <= 4546 {
+                albums.add(album)
+            }
+        }
+        print("👅 albums.count: \(albums.count)")
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetCollectionChangeRequest.deleteAssetCollections(albums)
+        }) { isSuccess, error in
+            if isSuccess {
+                print("🤗 success: delete album.")
+            } else {
+                print("😱 failure: \(error)")
+            }
+        }
     }
 }
 
